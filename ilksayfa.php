1490dd2e63143a9bca4090ffa654d53467d5767a
Esra Kelleci <83169230+esrakllci@users.noreply.github.com>
2022-05-18 23:27:42 +0300

    Add files via upload

---
+++ ilksayfa.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+ <meta charset="UTF-8">
+ <title>Merhaba PHP</title>
+</head>
+<body>
+ <?php echo "Merhaba"; ?>
+ <?php echo "Merhaba"."<b>PHP </b>"; ?>
+ <?php echo  "Merhaba". "2022"."yılı";?>
+  <?php echo "Merhaba PHP";?>
+  <?php echo "Merhaba 2022";?>
+  <?php echo "Merhaba PHP <br/>";?>
+  <?php echo "Merhaba 2022";?>
+  <?php
+   // echo "1,";
+        echo "2,";
+        /* echo "3,";
+        echo "4,";
+        echo "5,";
+        */
+        echo "6,";
+     ?>
+    <?php 
+      $degisken1=10;
+      $degisken1=$degisken1 + 20;
+      echo $degisken1;
+      $degisken1="Merhaba" . 20;
+      echo "$degisken1";
+      ?>
+
+</body>
+</html>
