dd909e055d05d9081ae45a139709797404ab6d79
Esra Kelleci <83169230+esrakllci@users.noreply.github.com>
2022-05-19 00:09:42 +0300

    Add files via upload

---
+++ ornekdevam.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+ <meta charset="UTF-8">
+ <title>PHP EXAMP</title>
+</head>
+<body>         
+<?php define("SATIR",50);
+      define("HATAMESAJI","Hatalı değer girdiniz.");
+             echo SATIR."<br/>";
+             echo HATAMESAJI;
+             ?>
+</body>
+</html>
